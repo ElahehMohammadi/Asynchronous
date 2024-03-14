8eacd740bbb7fb65accecda6d2681091d150d241
evie <e.v.221b@gmail.com>
2024-03-14 18:12:16 +0330

    till session 006 (005 is not finished yet)

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/name/south korea");
+request.send();
+
+request.addEventListener("load", function () {
+  console.log(this.responseText);
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `       
+    <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.kor}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.KRW.name
+            }</p>
+          </div>
+      </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+});
