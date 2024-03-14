f07c0cd9e6f91cb134c02537dd915ba563c6fe23
evie <e.v.221b@gmail.com>
2024-03-14 22:21:02 +0330

    till session 007

--- script.js
+++ script.js
@@ -4,30 +4,37 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/south korea");
-request.send();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener("load", function () {
-  console.log(this.responseText);
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener("load", function () {
+    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `       
+    const html = `       
     <article class="country">
-          <img class="country__img" src="${data.flag}" />
+          <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
+            <h3 class="country__name">${Object.values(data.name)[0]}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${(
               +data.population / 1000000
             ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.kor}</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
             <p class="country__row"><span>💰</span>${
-              data.currencies.KRW.name
+              Object.values(data.currencies)[0].name
             }</p>
           </div>
       </article>`;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData("iran");
+getCountryData("japan");
+getCountryData("south korea");
