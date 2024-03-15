e12c85aaf5e3571c3abf1af532d613dc7ff60392
evie <e.v.221b@gmail.com>
2024-03-16 00:20:22 +0330

    till session 008

--- script.js
+++ script.js
@@ -4,18 +4,44 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener("load", function () {
+//     console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `       
-    <article class="country">
+//     const html = `
+//     <article class="country">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${Object.values(data.name)[0]}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${
+//               Object.values(data.languages)[0]
+//             }</p>
+//             <p class="country__row"><span>💰</span>${
+//               Object.values(data.currencies)[0].name
+//             }</p>
+//           </div>
+//       </article>`;
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+// getCountryData("iran");
+// getCountryData("japan");
+// getCountryData("south korea");
+
+const renderCountry = function (data, className = "") {
+  const html = `       
+    <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
             <h3 class="country__name">${Object.values(data.name)[0]}</h3>
@@ -31,10 +57,37 @@ const getCountryData = function (country) {
             }</p>
           </div>
       </article>`;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+///////////////////////////////////////
+const getCountryANDneighbour = function (country) {
+  //ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+    //get neighbour country7
+    const neighbour = data.borders;
+    if (!neighbour) return;
+
+    //ajax call country 2
+    neighbour.forEach((el) => {
+      const request2 = new XMLHttpRequest();
+      request2.open("GET", `https://restcountries.com/v3.1/alpha/${el}`);
+      request2.send();
+      request2.addEventListener("load", function () {
+        const [data2] = JSON.parse(this.responseText);
+        console.log(data2);
+        renderCountry(data2, "neighbour");
+      });
+    });
   });
 };
-getCountryData("iran");
-getCountryData("japan");
-getCountryData("south korea");
+// getCountryANDneighbour("iran");
+// getCountryANDneighbour("japan");
+getCountryANDneighbour("south korea");
