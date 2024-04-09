5fa81a1bb8b7617876a0565dba058889b328a77d
evie <e.v.221b@gmail.com>
2024-04-10 02:15:23 +0330

    till session 012

--- script.js
+++ script.js
@@ -2,7 +2,32 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
-
+const renderCountry = function (data, className = "") {
+  console.log(data);
+  const html = `       
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${Object.values(data.name)[0]}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+      </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  // countriesContainer.style.opacity = 1;
+};
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  // countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest();
@@ -39,28 +64,8 @@ const countriesContainer = document.querySelector(".countries");
 // getCountryData("japan");
 // getCountryData("south korea");
 
-const renderCountry = function (data, className = "") {
-  const html = `       
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${Object.values(data.name)[0]}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${
-              Object.values(data.languages)[0]
-            }</p>
-            <p class="country__row"><span>💰</span>${
-              Object.values(data.currencies)[0].name
-            }</p>
-          </div>
-      </article>`;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
 ///////////////////////////////////////
+/*
 const getCountryANDneighbour = function (country) {
   //ajax call country 1
   const request = new XMLHttpRequest();
@@ -90,4 +95,48 @@ const getCountryANDneighbour = function (country) {
 };
 // getCountryANDneighbour("iran");
 // getCountryANDneighbour("japan");
-getCountryANDneighbour("south korea");
+getCountryANDneighbour("south korea");*/
+
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+// const req = fetch("https://restcountries.com/v3.1/name/canada");
+// console.log(req);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  //country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      if (!data[0].borders) return;
+      const neighbour = data[0].borders[0];
+
+      //country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then(([data]) => renderCountry(data, "neighbour"))
+    .catch((error) => {
+      console.error(error);
+      renderError("something went wrong try again!");
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+      // for loading circels
+    });
+};
+getCountryData("usa");
